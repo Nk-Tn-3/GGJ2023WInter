e2efe23c2d917357d0fef45dec3f7e7f28a9511c
Computer Science Guest 238 <zzz238@usask.ca>
2023-01-07 18:09:20 -0600

    some basic post processing. countdown timer for game

--- Assets/scripts/Gameplay/GameController.cs
+++ Assets/scripts/Gameplay/GameController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    //test
+    public float gameDurationTime = 10;
+    public Text timerTXT;
 
     private void Update()
     {
+        // hotkey to reset the current scene
         if (Input.GetKeyDown(KeyCode.T))
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.
                 SceneManager.GetActiveScene().name);
+
+        // countdown timer logic and UI updates
+        if(gameDurationTime > 0)
+        {
+            gameDurationTime -= Time.deltaTime;
+            timerTXT.text = Mathf.FloorToInt(gameDurationTime / 60) + ":" + Mathf.FloorToInt(gameDurationTime % 60);
+        }
+        else // timer has expired
+        {
+            print("game done");
+        }
     }
+
+
 }
